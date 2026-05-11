08db8e2d81e8ddf43b9f050cb547963fd906cdd5
labtrack9-cyber <labtrack4@gmail.com>
2026-05-11 14:46:59 +0530

    Fixed media files

--- elitecart/urls.py
+++ elitecart/urls.py
@@ -9,6 +9,11 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+)
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
